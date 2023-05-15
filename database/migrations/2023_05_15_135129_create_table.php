8a6838243323d3896c771f7924e002b19040291d
Alfiki Diastama Afan Firdaus <alfiki.diastama@gmail.com>
2023-05-15 22:27:56 +0700

    Add migrations

--- database/migrations/2023_05_15_135129_create_table.php
+++ database/migrations/2023_05_15_135129_create_table.php
@@ -11,6 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id')->primary();
+            $table->string('name', 100);
+            $table->string('email', 50);
+            $table->string('role', 50);
+            $table->string('no_telp', 15);
+            $table->string('password', 20);
+            $table->timestamps();
+        });
+
+        Schema::create('administrators', function (Blueprint $table) {
+            $table->id('administrator_id')->primary();
+            $table->string('name', 100);
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+
         Schema::create('unit_admins', function (Blueprint $table) {
             $table->id('unit_admin_id')->primary();
             $table->foreign('unit_id')->references('unit_id')->on('units')->onDelete('cascade');
@@ -20,26 +37,43 @@ return new class extends Migration
 
         Schema::create('units', function (Blueprint $table) {
             $table->id('unit_id')->primary();
-            $table->string('name', 50);
-            $table->string('location', 50);
+            $table->string('name', 100);
+            $table->string('location', 100);
             $table->timestamps();
         });
 
-        Schema::create('administrators', function (Blueprint $table) {
-            $table->integer('administrator_id')->primary();
-            $table->string('name', 50);
-            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id('booking_id')->primary();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status', ['pending','waiting', 'approved', 'rejected', 'canceled'])->default('pending');
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
 
-        Schema::create('users', function (Blueprint $table) {
-            $table->integer('user_id')->primary();
+        Schema::create('usage', function (Blueprint $table) {
+            $table->id('usage_id')->primary();
+            $table->string('note_text', 500);
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('items', function (Blueprint $table) {
+            $table->id('item_id')->primary();
+            $table->string('name', 100);
+            $table->string('brand', 50);
+            $table->string('serial_number', 50);
+            $table->string('photo', 100);
+            $table->string('description', 500);
+            $table->enum('status', ['pending','waiting', 'approved', 'rejected', 'canceled'])->default('pending');
+            $table->foreign('categories_id')->references('id')->on('categories')->onDelete('cascade');;
+            $table->foreign('unit_id')->references('id')->on('unit')->onDelete('cascade');;
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id('categories_id')->primary();
             $table->string('name');
-            $table->string('email');
-            $table->string('role');
-            $table->string('no_telp');
-            $table->string('password');
-            $table->timestamps();
         });
 
     }
@@ -49,6 +83,13 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('administrators');
+        Schema::dropIfExists('unit_admins');
+        Schema::dropIfExists('units');
+        Schema::dropIfExists('bookings');
+        Schema::dropIfExists('usage');
+        Schema::dropIfExists('items');
+        Schema::dropIfExists('categories');
     }
 };
