09a887648a83002cf968b358752a0aa0d9701e17
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-05-23 09:32:41 +0700

    Change to Sipinjam

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -106,7 +106,7 @@
             <div class="col-lg-6">
               <div class="d-flex flex-column h-100">
                 <p class="mb-1 pt-2 text-bold">Built by developers</p>
-                <h5 class="font-weight-bolder">Soft UI Dashboard</h5>
+                <h5 class="font-weight-bolder">Dashboard - Sipinjam</h5>
                 <p class="mb-5">From colors, cards, typography to complex elements, you will find the full documentation.</p>
                 <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
                   Read More
--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -188,7 +188,6 @@
               </g>
             </svg>
           </div>
-          <span class="nav-link-text ms-1">Sign In</span>
         </a>
       </li>
       <li class="nav-item">
@@ -210,24 +209,8 @@
               </g>
             </svg>
           </div>
-          <span class="nav-link-text ms-1">Sign Up</span>
         </a>
       </li>
     </ul>
-  </div>
-  <div class="sidenav-footer mx-3 ">
-    <div class="card card-background shadow-none card-background-mask-secondary" id="sidenavCard">
-      <div class="full-background" style="background-image: url('../assets/img/curved-images/white-curved.jpeg')"></div>
-      <div class="card-body text-start p-3 w-100">
-        <div class="icon icon-shape icon-sm bg-white shadow text-center mb-3 d-flex align-items-center justify-content-center border-radius-md">
-          <i class="ni ni-diamond text-dark text-gradient text-lg top-0" aria-hidden="true" id="sidenavCardIcon"></i>
-        </div>
-        <div class="docs-info">
-          <h6 class="text-white up mb-0">Need help?</h6>
-          <p class="text-xs font-weight-bold">Please check our docs</p>
-          <a href="/documentation/getting-started/overview.html" target="_blank" class="btn btn-white btn-sm w-100 mb-0">Documentation</a>
-        </div>
-      </div>
-    </div>
   </div>
 </aside>
