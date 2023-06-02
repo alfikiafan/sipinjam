c256ce3b0fbc07f65dafe07898ce1b4077c25225
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-06-02 20:11:39 +0700

    Afan - make Model:Booking, Usage

---
+++ app/Models/Booking.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'item_id',
+        'user_id',
+        'start_date',
+        'end_date',
+        'status'
+    ];
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function usage()
+    {
+        return $this->hasOne(Usage::class);
+    }
+}
