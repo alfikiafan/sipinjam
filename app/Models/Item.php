6a40de16db7e88c3bcd3794d027fdab5608c87e4
Haqqi Setiadjie <haqqisetiadjie@student.uns.ac.id>
2023-06-02 20:18:53 +0700

    model item, unit, category

---
+++ app/Models/Item.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'categories_id', 
+        'unit_id', 
+        'name', 
+        'brand', 
+        'serial_number', 
+        'photo', 
+        'description', 
+        'status'
+    ];
+
+    public function Category()
+    {
+        return $this->belongsTo(Category::class, 'categories_id');
+    }
+
+    public function Unit()
+    {
+        return $this->belongsTo(Unit::class);
+    }
+
+    public function Bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+}
