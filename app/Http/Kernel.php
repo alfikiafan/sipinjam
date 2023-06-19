dab20781bffba0754aa7bead75abb0b61f2f49fa
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-06-19 16:05:42 +0700

    Add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'administrator' => \App\Http\Middleware\AdministratorMiddleware::class,
+        'unitadmin' => \App\Http\Middleware\UnitAdminMiddleware::class,
+        'borrower' => \App\Http\Middleware\BorrowerMiddleware::class,
     ];
 }
